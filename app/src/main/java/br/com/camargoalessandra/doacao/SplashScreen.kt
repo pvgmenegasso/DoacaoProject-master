37d53277e8c33d38e41dfc1525916968289f5d52
DESKTOP-63NOP9H\pvnan <pvgmenegasso@gmail.com>
2018-11-07 22:04:53 -0200

    splash screen (ainda sem funcionar internet)

---
+++ app/src/main/java/br/com/camargoalessandra/doacao/SplashScreen.kt
@@ -0,0 +1,28 @@
+package br.com.camargoalessandra.doacao
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        GlideApp.with (this)
+                .load("https://drive.google.com/file/d/1K98NjY1stdaJ1zQTLT268xwiwEG64rSw/view")
+                //.placeholder(R.mipmap.space)
+                .into(imgLogo)
+
+        Handler().postDelayed({
+            val main = Intent(this, MainActivity::class.java)
+            startActivity(main)
+            finish()
+        }, 2000)
+    }
+}
